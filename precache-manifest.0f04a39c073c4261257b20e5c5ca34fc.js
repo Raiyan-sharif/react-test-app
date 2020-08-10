bc719621819c9e3cf633d206c617a96cc85c33e9
Raiyan <raiyan.sharif.1234@gmail.com>
2020-08-10 23:18:36 +0600

    Updates

---
+++ precache-manifest.0f04a39c073c4261257b20e5c5ca34fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec72499636b7b305827821592898348d",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "4ea52349c92cffd91c21",
+    "url": "/react-test-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6abb5a9a12b95a6c32d7",
+    "url": "/react-test-app/static/js/2.d510430e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test-app/static/js/2.d510430e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea52349c92cffd91c21",
+    "url": "/react-test-app/static/js/main.62b9a1aa.chunk.js"
+  },
+  {
+    "revision": "3c6c5f833308f6340305",
+    "url": "/react-test-app/static/js/runtime-main.f2e8b6ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
